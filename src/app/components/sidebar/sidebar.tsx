5a9db18209b0a6e238e31811404643a6cf96a9e8
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-10 11:30:48 -0800

    Create sidebar.tsx

---
+++ src/app/components/sidebar/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar(): React.ReactElement<nav> {
+  return <nav 
+           className="float-left w-1/5 
+                      bg-sky-800 
+                      border-solid border-r-4 border-cyan-400"
+         />;
+}
