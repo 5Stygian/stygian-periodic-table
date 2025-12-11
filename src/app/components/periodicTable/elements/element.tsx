661bc252fd12895a4ed4da0c1a1081da7d8804c6
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 12:48:13 -0800

    Update element.tsx

--- src/app/components/periodicTable/elements/element.tsx
+++ src/app/components/periodicTable/elements/element.tsx
@@ -14,7 +14,7 @@ export default function Element({
   atomicNumber,
   color,
 }: ElementProps) {
-  const blockStyle = `min-w-25 min-h-25 border-2 shadow-2x1 shadow-${color} text-${color}`;
+  const blockStyle = `min-w-25 min-h-25 border-2 shadow-2x1 shadow-${color} text-${color} scale-75`;
 
   return (
     <div className={blockStyle}>
