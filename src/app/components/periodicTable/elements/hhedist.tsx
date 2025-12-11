835ec6db0cfd197c42a4dd60116eed6184c97117
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 12:44:57 -0800

    Update hhedist.tsx

--- src/app/components/periodicTable/elements/hhedist.tsx
+++ src/app/components/periodicTable/elements/hhedist.tsx
@@ -2,21 +2,23 @@ import EmptyCell from './emptyCell';
 
 export default function HydrogenHeliumDist() {
   retrun (
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
-    <EmptyCell />
+    <>      
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+      <EmptyCell />
+    </>
   );
 }
