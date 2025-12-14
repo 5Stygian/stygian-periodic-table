0f3e2ab99796861d31bfa7905e4f39f77ae50353
5Stygian <rlcrafistohard@gmail.com>
2025-12-14 12:43:34 -0800

    change cursor style on hover

--- src/app/components/periodicTable/cell/cell.tsx
+++ src/app/components/periodicTable/cell/cell.tsx
@@ -91,7 +91,7 @@ export default function Cell({
           break;
       } // switch
 
-      blockStyle = `${blockStyle} ${color} duration-75 ease-[cubic-bezier(0.06, 0.98, 0.41, 0.93)] hover:scale-105 hover:z-10`;
+      blockStyle = `${blockStyle} ${color} duration-75 ease-[cubic-bezier(0.06, 0.98, 0.41, 0.93)] hover:cursor-default hover:scale-105 hover:z-10`;
 
       return (
         <div className={blockStyle}>
