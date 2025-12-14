e3b9a6b860cdf19c859ce3734f8b4046c8a6de4c
5Stygian <rlcrafistohard@gmail.com>
2025-12-13 18:57:12 -0800

    start on making ts more data driven

---
+++ src/app/data/elements.ts
@@ -0,0 +1,43 @@
+let ELEMENTS: Element[] = [];
+
+class Element {
+    /**public constructor(
+        public readonly name        : string,
+        public readonly symbol      : string,
+        public readonly family      : string,
+        public readonly atomicNumber: number,
+        public readonly group       : number,
+        public readonly period      : number,
+    ) {
+        ELEMENTS.push(this);
+    }*/
+    public constructor({name, symbol, family, atomicNumber, group, period}: {
+        name: string,
+        symbol: string,
+        family: string,
+        atomicNumber: number,
+        group: number,
+        period: number,
+    }
+    ) {
+        ELEMENTS.push(this);
+    }
+}
+
+// period 1
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 1, group: 1, period: 1});
+export const Helium: Element = new Element({name: "Helium", symbol: "He", family: "nobleGas", atomicNumber: 2, group: 18, period: 1});
+
+/**
+// period 2
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 3, group: 1, period: 2});
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 4, group: 1, period: 2});
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 5, group: 1, period: 2});
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 6, group: 1, period: 2});
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 7, group: 1, period: 2});
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 8, group: 1, period: 2});
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 9, group: 1, period: 2});
+export const Hydrogen: Element = new Element({name: "Hydrogen", symbol: "H", family: "nonmetal", atomicNumber: 10, group: 1, period: 2});
+*/
+
+export default ELEMENTS;
