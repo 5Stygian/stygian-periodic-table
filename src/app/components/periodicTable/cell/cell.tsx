4d226ab999807e553f64e79543e1b857f8520b30
5Stygian <rlcrafistohard@gmail.com>
2025-12-14 13:20:29 -0800

    change border size

--- src/app/components/periodicTable/cell/cell.tsx
+++ src/app/components/periodicTable/cell/cell.tsx
@@ -40,7 +40,7 @@ export default function Cell({
       </div>
     ); // return
   } else if (!empty) {
-    let blockStyle = `${defaultCellStyles} bg-black border-2 shadow-2x1 font-bold`;
+    let blockStyle = `${defaultCellStyles} bg-black border-3 shadow-2x1 font-bold`;
 
     if (!compressedFBlock) {
       blockStyle = `${blockStyle} ${element!.color} duration-75 ease-[cubic-bezier(0.06, 0.98, 0.41, 0.93)] hover:cursor-default hover:scale-105 hover:z-10`;
