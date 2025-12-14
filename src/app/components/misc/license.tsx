8ffcfe6a856a7653dfbe37e9b8a1b501b71a3870
5Stygian <rlcrafistohard@gmail.com>
2025-12-14 15:47:24 -0800

    add padding in license

--- src/app/components/misc/license.tsx
+++ src/app/components/misc/license.tsx
@@ -1,6 +1,6 @@
 export default function License() {
   return (
-    <div>
+    <div className="pl-4">
       The content of this page is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en" target="_blank" className="text-sky-400 underline">Creative Commons Attribution Non Commercial Share Alike 4.0 International</a>
     </div>
   );
