7cfa59b58d5ec3d24a5dc7cec50ce6f279d8570a
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-15 12:31:27 -0800

    change width of card

--- src/app/components/elementCard/elementCard.tsx
+++ src/app/components/elementCard/elementCard.tsx
@@ -2,9 +2,10 @@ import { NaE } from "@/app/data/elements";
 
 export default function ElementCard() {
   const symbolStyling = `min-w-32 w-min min-h-40 bg-black border-3 shadow-2x1 font-bold ${NaE.color} flex justify-center items-center hover:cursor-default`;
-  const dataTableStyling = `min-w-110 w-min min-h-40 bg-black border-3 shadow-2x1 font-bold ${NaE.color} flex justify-center items-center ml-8 px-12 hover:cursor-default`;
-  const dataTitleStyling = "min-w-40 pr-12 float-left";
-
+  const dataTableStyling = `min-w-250 w-min min-h-40 bg-black border-3 shadow-2x1 font-bold ${NaE.color} flex justify-center items-center ml-8 px-12 hover:cursor-default`;
+  const dataTitleStyling = "pr-12 float-left";
+  const dataContentStyling = "text-white";
+  
   return (
     <div id="elementCard" className="w-screen flex justify-center items-center pb-20 hover:cursor-default">
       <div className={symbolStyling}>
@@ -15,36 +16,36 @@ export default function ElementCard() {
           <tbody>
             <tr>
               <th className={dataTitleStyling}>Name</th>
-              <td id="elementCardName" className="text-white">{NaE.name}</td>
+              <td id="elementCardName" className={dataContentStyling}>{NaE.name}</td>
             </tr>
             <tr>
               <th className={dataTitleStyling}>Family</th>
-              <td id="elementCardFamily" className="text-white">{NaE.familyText}</td>
+              <td id="elementCardFamily" className={dataContentStyling}>{NaE.familyText}</td>
             </tr>
             <tr>
               <th className={dataTitleStyling}>Atomic Number</th>
-              <td id="elementCardAtomicNumber" className="text-white">{NaE.atomicNumber}</td>
+              <td id="elementCardAtomicNumber" className={dataContentStyling}>{NaE.atomicNumber}</td>
             </tr>
             <tr>
               <th className={dataTitleStyling}>Group</th>
-              <td id="elementCardGroup" className="text-white">{NaE.group}</td>
+              <td id="elementCardGroup" className={dataContentStyling}>{NaE.group}</td>
             </tr>
             <tr>
               <th className={dataTitleStyling}>Period</th>
-              <td id="elementCardPeriod" className="text-white">{NaE.period}</td>
+              <td id="elementCardPeriod" className={dataContentStyling}>{NaE.period}</td>
             </tr>
             <br />
             <tr>
-              <th className="text-white" colSpan={2}>Electron Configuration</th>
+              <th className={dataContentStyling} colSpan={2}>Electron Configuration</th>
             </tr>
             <br />
             <tr>
               <th className={dataTitleStyling}>Full</th>
-              <td id="elementCardECFull" className="text-white">{NaE.fullElectronConfiguration}</td>
+              <td id="elementCardECFull" className={dataContentStyling}>{NaE.fullElectronConfiguration}</td>
             </tr>
             <tr>
               <th className={dataTitleStyling}>Noble Gas</th>
-              <td id="elementCardECNobleGas" className="text-white">{NaE.nobleGasElectronConfiguration}</td>
+              <td id="elementCardECNobleGas" className={dataContentStyling}>{NaE.nobleGasElectronConfiguration}</td>
             </tr>
           </tbody>
         </table>
