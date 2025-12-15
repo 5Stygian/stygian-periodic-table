181fee2e7d60e7edb4ea5a210cf5c2a2c30ff0e7
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-15 10:14:33 -0800

    Update license.tsx

--- src/app/components/misc/license.tsx
+++ src/app/components/misc/license.tsx
@@ -1,7 +1,7 @@
 export default function License() {
   return (
     <div className="pl-4">
-      The content of this page is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en" target="_blank" className="text-sky-400 underline">Creative Commons Attribution Non Commercial Share Alike 4.0 International</a>
+      The content of this page is licensed under <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.en" target="_blank" className="text-sky-400 underline">Creative Commons Zero v1.0 Universal</a>
     </div>
   );
 }
